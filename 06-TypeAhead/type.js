7d7dc32540c07b1c8cb2dede6fa52d53413c6fb4
Lindsay Lopez <lopez95@berkeley.edu>
2018-12-12 08:46:23 -0800

    Added highligting feature and cleaned up the code

--- 06-TypeAhead/type.js
+++ 06-TypeAhead/type.js
@@ -7,27 +7,50 @@ fetch(endpoint)
 .then(blob => blob.json())
 .then(data => cities.push(...data));
 
+function resetChildren() {
+  let resultsContainer = document.querySelector('.result-container');
+  
+  let initialChildrenHTML = `
+    <li class="result">Filter For A City</li>
+    <li class="result">Or A State</li>
+  `;
+  
+  resultsContainer.innerHTML = initialChildrenHTML;
+}
+
 // Given a word, find the corresponding cities that match it
 function findMatches(wordToMatch, cities) {
-  // new RegEx(wordToMatch, 'gi') => string.match(regex)
-  const matches = cities.filter(city => city.city.toLowerCase().includes(wordToMatch) || city.state.toLowerCase().includes(wordToMatch));
+  const regex = new RegExp(wordToMatch, 'gi');
+  const matches = cities.filter(city => regex.test(city.city) || regex.test(city.state));
   return matches;
 }
 
 // Show our matches
-function showMatches(matches) {
-  let resultsContainer = document.querySelector('.result-container');
-  while (resultsContainer.firstChild) {
-    resultsContainer.removeChild(resultsContainer.firstChild);
+function showMatches() {
+  if (!this.value) {
+    resetChildren();
+    return;
   }
-  matches.forEach(city => {
-    let li = document.createElement('li');
-    li.classList.add('result');
-    li.innerHTML = `<span>${city.city}, ${city.state}</span><span>${city.population}</span>`;
-    resultsContainer.appendChild(li);
-  });
+  let resultsContainer = document.querySelector('.result-container');
+  const matches = findMatches(this.value, cities);
+  let matchesHTML = matches.map(city => {
+    const regex = new RegExp(this.value, 'gi');
+    const cityName = city.city.replace(regex, `<span class="highlight">${this.value}</span>`);
+    const stateName = city.state.replace(regex, `<span class="highlight">${this.value}</span>`);
+    return `
+      <li class="result">
+        <span class="name">${cityName}, ${stateName}</span>
+        <span class="population">${city.population}</span>
+      </li>
+    `;
+  }).join('');
+
+  resultsContainer.innerHTML = matchesHTML;
 }
 
+document.querySelector('.search').addEventListener('input', showMatches);
+
+resetChildren();
 
 
 
