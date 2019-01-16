24dedb500cefc2328d8990a7e1ce230a1ec9e9b8
Lindsay Lopez <lopez95@berkeley.edu>
2019-01-15 23:41:29 -0800

    Added play/pause functionality.

--- 11-HTMLVideoPlayer/script.js
+++ 11-HTMLVideoPlayer/script.js
@@ -1,7 +1,25 @@
+// Variables
+const video = document.querySelector('video');
+let playVideo = false;
+
+// Displays controls when we hover our mouse over the video
 document.querySelector('.video-box').addEventListener('mouseover', (e) => {
   document.querySelector('.controls').classList.add('hover');
 });
-
 document.querySelector('.video-box').addEventListener('mouseout', (e) => {
   document.querySelector('.controls').classList.remove('hover');
 });
+
+// Event Listeners
+document.querySelector('.play').addEventListener('click', toggleVideo);
+
+
+// Functions
+function toggleVideo(event) {
+  if (playVideo) {
+    video.pause();
+  } else {
+    video.play();
+  }
+  playVideo = !playVideo;
+}
